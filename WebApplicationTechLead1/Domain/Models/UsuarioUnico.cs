f24011b56b8e65a8eba8dfcba3ff4e65f9f29d23
IgorDAzevedo <igor.dazevedo@peakinvest.com.br>
2024-05-19 09:25:14 -0300

    padrao criacional > thread safe > instance with Lock na classe UsuarioUnico

---
+++ WebApplicationTechLead1/Domain/Models/UsuarioUnico.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplicationTechLead1.Domain.Models
+{
+    public class UsuarioUnico
+    {
+        private static UsuarioUnico _instance = null;
+        private static readonly object _instanceLock = new object();
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public List<Cliente> _cliente { get; set; }
+        public static UsuarioUnico GetInstance()
+        {
+            if (_instance != null) return _instance;
+            lock (_instanceLock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new UsuarioUnico();
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
