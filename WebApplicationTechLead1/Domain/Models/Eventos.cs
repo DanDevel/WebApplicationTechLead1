a9364d71ababaf0abe4994d2ea783f9e00b3f7f9
IgorDAzevedo <igor.dazevedo@peakinvest.com.br>
2024-05-19 08:35:18 -0300

    entity Eventos adicionada ao EntityFramework

---
+++ WebApplicationTechLead1/Domain/Models/Eventos.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplicationTechLead1.Domain.Models
+{
+    public class Eventos
+    {
+        public Guid Id { get; set; }
+        public string EventName { get; set; }
+        public string EventType { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public Eventos() { }
+
+        public Eventos(Guid id, string eventName, string eventType, string eventDescription, DateTime eventDate)
+        {
+            Id = id;
+            EventName = eventName;
+            EventType = eventType;
+            EventDescription = eventDescription;
+            EventDate = eventDate;
+        }
+
+    }
+}
